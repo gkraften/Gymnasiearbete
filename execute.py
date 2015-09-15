d4f57f9080641770432e619cc486ab2eecac7353
gkraften <g.spam32@gmail.com>
2015-09-15 17:25:01 +0200

    Lade till experiment med avståndssensorn

--- execute.py
+++ execute.py
@@ -1,12 +1,12 @@
 import tkinter.simpledialog as dialog
 from tkinter import Tk
-import os
 from subprocess import call
 import sys
+
 
 root = Tk()
 root.lift()
 if sys.platform == "darwin":
-    call(["osascript", "-e", 'tell app "Finder" to set frontmost of process "Python" to true'])
+    call(["osascript", "-e", 'tell app "Finder" to set frontmost of process "Robot" to true'])
 root.withdraw()
 print(dialog.askstring("Titel", "Undertitel"))
