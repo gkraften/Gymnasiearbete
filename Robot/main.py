7a20deabcd2ecbda5d5bed0ca28deb68f01eae34
Oscar <g.spam32@gmail.com>
2015-09-15 18:40:33 +0200

    Fixade grejer

--- Robot/main.py
+++ Robot/main.py
@@ -1,1 +1,1 @@
-print("cancerböldpestkolera")
+printa("cancerböldpestkolera")
--- execute.py
+++ execute.py
@@ -2,17 +2,36 @@ from subprocess import call
 import sys
 
 
-command = '''cd Desktop
+command = '''\
+#!/bin/bash
+type=daemon
+cd Desktop
 if [ -d Gymnasiearbete ]; then
     cd Gymnasiearbete
     git pull
+    if [ $? -ne 0 ]; then
+        echo Något blev fel när projektet skulle synkas med github
+        echo -n Tryck på enter för att avsluta
+        read
+        exit
+    fi
 else
     git clone https://github.com/gkraften/Gymnasiearbete.git
+    if [ $? -ne 0 ]; then
+        echo Något blev fel när projektet skulle hämtas från github
+        echo -n Tryck på enter för att avsluta
+        read
+        exit
+    fi
     cd Gymnasiearbete
 fi
 
 cd Robot
-sudo python3 main.py'''
+if [[ $type == daemon ]]; then
+    sudo python3 main.py > /home/pi/Desktop/meddelanden.txt 2> /home/pi/Desktop/fel.txt &
+else
+    sudo python3 main.py || read
+fi'''
 
 if sys.platform == "darwin":
     applescript = ('if application "Terminal" is running then\n'
@@ -26,6 +45,6 @@ if sys.platform == "darwin":
                    '		do script "command" in window 1\n'
                    '	end tell\n'
                    'end if')
-    call(["osascript", "-e", applescript.replace("command", "ssh pi@raspberrypi.local '{}'; read; exit".format(command))])
+    call(["osascript", "-e", applescript.replace("command", "ssh pi@raspberrypi.local '{}'; exit".format(command))])
 else:
     print("Det fungerar inte på den här datorn än")
