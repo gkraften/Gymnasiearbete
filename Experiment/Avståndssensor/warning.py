c8466d623b0590abc72cd2b9b4fe9774061eb8c1
Oscar <g.spam32@gmail.com>
2015-09-16 08:11:50 +0200

    Lade till ett experiment

---
+++ Experiment/Avståndssensor/warning.py
@@ -0,0 +1,44 @@
+import RPi.GPIO as GPIO
+import time
+
+
+def distance():
+    GPIO.output(trig, 1)
+    time.sleep(0.00001)
+    GPIO.output(trig, 0)
+
+    while GPIO.input(echo) == 0:
+        pass
+    start = time.time()
+
+    while GPIO.input(echo) == 1:
+        pass
+
+    stop = time.time()
+
+    return int(round((stop - start) * 17150, 0))
+
+
+trig = 23
+echo = 24
+led = 18
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(trig, GPIO.OUT, initial=False)
+GPIO.setup(led, GPIO.OUT, initial=False)
+GPIO.setup(echo, GPIO.IN)
+
+time.sleep(0.1)
+
+try:
+    while True:
+        if distance() < 20:
+            GPIO.output(led, True)
+        elif distance >= 20:
+            GPIO.output(led, False)
+        time.sleep(0.1)
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
