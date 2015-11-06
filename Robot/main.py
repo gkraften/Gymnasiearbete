b8189c4308d03eff63d622bcf45e17e26878d26e
gkraften <g.spam32@gmail.com>
2015-11-06 16:59:01 +0100

    Program för att läsa av om batteriet är på väg att ta slut

--- Robot/main.py
+++ Robot/main.py
@@ -1,1 +1,14 @@
-print("cancerböldpestkolera")
+# Programmet ser ständigt efter om batteriet håller på
+# att ta slut och stänger i så fall av datorn.
+
+import robot
+import time
+import RPi.GPIO as GPIO
+
+while True:
+    if robot.is_battery_low():
+        with open("/home/pi/Desktop/halt.txt", "w") as f:
+            print("Batteriet är lågt! Stänger av.", file=f)
+        GPIO.cleanup()
+        robot.halt()
+    time.sleep(1)
