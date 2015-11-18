212a33b6018a767ab0c34caefaf2727be0f5cfc6
Oscar <g.spam32@gmail.com>
2015-11-18 17:11:51 +0100

    Testkörning1

--- Robot/main.py
+++ Robot/main.py
@@ -2,14 +2,10 @@ import robot.motors
 import time
 import RPi.GPIO as GPIO
 
-robot.motors.LEFT.forward()
-time.sleep(1)
-robot.motors.LEFT.backward()
-time.sleep(1)
-robot.motors.stop()
-robot.motors.RIGHT.forward()
-time.sleep(1)
-robot.motors.RIGHT.backward()
-time.sleep(1)
-robot.motors.stop()
-GPIO.cleanup()
+try:
+    robot.motors.LEFT.forward()
+    robot.motors.RIGHT.forward()
+    while True:
+        pass
+finally:
+    GPIO.cleanup()
