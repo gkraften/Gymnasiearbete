b9886103227a42310a6bcb2f23be273ac0252862
Oscar <g.spam32@gmail.com>
2015-11-18 17:00:26 +0100

    Motorer

--- Robot/main.py
+++ Robot/main.py
@@ -1,14 +1,15 @@
-# Programmet ser ständigt efter om batteriet håller på
-# att ta slut och stänger i så fall av datorn.
-
-import robot
+import robot.motors
 import time
 import RPi.GPIO as GPIO
 
-while True:
-    if robot.is_battery_low():
-        with open("/home/pi/Desktop/halt.txt", "w") as f:
-            print("Batteriet är lågt! Stänger av.", file=f)
-        GPIO.cleanup()
-        robot.halt()
-    time.sleep(1)
+robot.motors.LEFT.forward()
+time.sleep(1)
+robot.motors.LEFT.backward()
+time.sleep(1)
+robot.motors.stop()
+robot.motors.RIGHT.forward()
+time.sleep(1)
+robot.motors.RIGHT.backward()
+time.sleep(1)
+robot.motors.stop()
+GPIO.cleanup()
---
+++ Robot/robot/motors.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+
+class Motor:
+    def __init__(self, pin1, pin2):
+        self.pin1 = pin1
+        self.pin2 = pin2
+
+        GPIO.setup(pin1, GPIO.OUT)
+        GPIO.setup(pin2, GPIO.OUT)
+
+    def forward(self):
+        GPIO.output(self.pin1, True)
+        GPIO.output(self.pin2, False)
+
+    def backward(self):
+        GPIO.output(self.pin2, True)
+        GPIO.output(self.pin1, False)
+
+    def stop(self):
+        GPIO.output(self.pin1, False)
+        GPIO.output(self.pin2, False)
+
+LEFT = Motor(22, 24)
+RIGHT = Motor(16, 18)
+
+def stop():
+    LEFT.stop()
+    RIGHT.stop()
