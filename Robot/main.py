f993204b35cbfc174759955040b7835d2cc345d0
Oscar <g.spam32@gmail.com>
2015-11-18 18:31:30 +0100

    smidigare

--- Robot/main.py
+++ Robot/main.py
@@ -2,9 +2,28 @@ import robot.motors
 import time
 import RPi.GPIO as GPIO
 
+#Ignorera den här funktionen. Jag vet knappt själv hur den funkar
+#/G
+def getch():
+    import termios
+    import sys, tty
+    def _getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+    return _getch
+
+get = getch()
+
 try:
     while True:
-        cmd = input("> ")
+        cmd = get()
         if cmd == "f":
             robot.motors.LEFT.forward()
             robot.motors.RIGHT.forward()
