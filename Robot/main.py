1b2d8f1514bb694fd22674bc8010d0f762b98ac5
Oscar <g.spam32@gmail.com>
2015-11-18 18:08:08 +0100

    Basic kontroll

--- Robot/main.py
+++ Robot/main.py
@@ -3,9 +3,15 @@ import time
 import RPi.GPIO as GPIO
 
 try:
-    robot.motors.LEFT.forward()
-    robot.motors.RIGHT.forward()
     while True:
-        pass
+        cmd = input("> ")
+        if cmd == "f":
+            robot.motors.LEFT.forward()
+            robot.motors.RIGHT.forward()
+        elif cmd == "b":
+            robot.motors.LEFT.backward()
+            robot.motors.RIGHT.backward()
+        elif cmd == "s":
+            robot.motors.stop()
 finally:
     GPIO.cleanup()
