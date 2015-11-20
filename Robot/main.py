b96059e8923ddb30b1eef8485bf376a57fc3f82f
Oscar <g.spam32@gmail.com>
2015-11-20 17:03:21 +0100

    Kortare avstånd

--- Robot/main.py
+++ Robot/main.py
@@ -11,15 +11,15 @@ robot.motors.forward()
 
 try:
     while True:
-        if (robot.distance.get_mid() < 30):
+        if (robot.distance.get_mid() < 20):
             random.choice([robot.motors.left, robot.motors.right])()
             time.sleep(0.5)
             robot.motors.forward()
-        if (robot.distance.get_left() < 30):
+        if (robot.distance.get_left() < 20):
             robot.motors.right()
             time.sleep(0.5)
             robot.motors.forward()
-        if (robot.distance.get_right() < 30):
+        if (robot.distance.get_right() < 20):
             robot.motors.left()
             time.sleep(0.5)
             robot.motors.forward()
