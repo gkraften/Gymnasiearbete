800b09d087fe5c97ce44c42be8f7e1e2c593517f
Oscar <g.spam32@gmail.com>
2015-11-20 16:53:17 +0100

    fix

--- Robot/main.py
+++ Robot/main.py
@@ -11,7 +11,7 @@ robot.on_battery_low(lambda: print("Lågt batteri!"))
 
 try:
     while True:
-        print("Mitten: " + robot.distance.get_mid())
+        print("Mitten: " + str(robot.distance.get_mid()))
         print("Vänster: " + robot.distance.get_left())
         print("Höger: " + robot.distance.get_right())
         time.sleep(0.1)
