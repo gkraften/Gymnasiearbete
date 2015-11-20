bd339999b5c741e509b6c87945529021b4afbc98
Oscar <g.spam32@gmail.com>
2015-11-20 15:53:25 +0100

    Avståndssensor

---
+++ Robot/robot/distance.py
@@ -0,0 +1,25 @@
+import robot.pins as pins
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setup(pins.DISTANCE_MID_TRIG, GPIO.OUT)
+GPIO.setup(pins.DISTANCE_MID_ECHO, GPIO.IN)
+
+def get(trig, echo):
+    GPIO.output(trig, 1)
+    time.sleep(0.00001)
+    GPIO.output(trig, 0)
+
+    while GPIO.input(echo) == 0:
+        pass
+    start = time.time()
+
+    while GPIO.input(echo) == 1:
+        pass
+
+    stop = time.time()
+
+    return (stop - start) * 17150
+
+def get_mid():
+    return get(pins.DISTANCE_MID_TRIG, pins.DISTANCE_MID_ECHOAvst)
