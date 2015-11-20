adee59d7667eadc7b65e6b4fe9f7363461a0b12b
Oscar <g.spam32@gmail.com>
2015-11-20 17:00:15 +0100

    Väggar

--- Robot/main.py
+++ Robot/main.py
@@ -7,13 +7,22 @@ import RPi.GPIO as GPIO
 
 robot.on_battery_low(lambda: print("Lågt batteri!"))
 
-#robot.motors.forward()
+robot.motors.forward()
 
 try:
     while True:
-        print("Mitten: " + str(robot.distance.get_mid()))
-        print("Höger: " + str(robot.distance.get_right()))
-        print("Vänster: " + str(robot.distance.get_left()))
+        if (robot.distance.mid() < 30):
+            random.choice([robot.motors.left, robot.motors.right])()
+            time.sleep(0.5)
+            robot.motors.forward()
+        if (robot.distance.left() < 30):
+            robot.motors.right()
+            time.sleep(0.5)
+            robot.motors.forward()
+        if (robot.distance.right() < 30):
+            robot.motors.left()
+            time.sleep(0.5)
+            robot.motors.forward()
         time.sleep(0.1)
 finally:
     GPIO.cleanup()
