c9fa3cc615178f0a812cb9864829ffb8f030ecc3
Oscar <g.spam32@gmail.com>
2015-11-27 18:48:36 +0100

    Kalibrering

--- Robot/calibrate_compass.py
+++ Robot/calibrate_compass.py
@@ -7,32 +7,24 @@ import sys
 compass.wake()
 compass.setHighSpeedDataRate()
 
-x_max = None
-x_min = None
-y_max = None
-y_min = None
+x_list = []
+y_list = []
 
 try:
     if len(sys.argv) == 2 and sys.argv[1] == "-a":
         motors.right(50)
 
-    x, y, z = compass.readAxisData()
-    x_max = x
-    x_min = x
-    y_max = y
-    y_min = y
     while True:
         x, y, z = compass.readAxisData()
-        x_max = max(x, x_max)
-        x_min = min(x, x_min)
-        y_max = max(y, y_max)
-        y_min = min(y, y_min)
 
-        print("{} {} {} {}".format(x_min, x_max, y_min, y_max))
+        x_list.appen(x)
+        y_list.append(y)
+        if (len(x_list) >= 2 and len(y_list) >= 2):
+            print("{} {} {} {}".format(min(x_list), max(x_list), min(y_list), max(y_list)))
 
         time.sleep(1/220)
 except KeyboardInterrupt:
-    print("x offset: {}\ty offset: {}".format((x_min + x_max)/2, (y_min + y_max)/2))
+    print("x offset: {}\ty offset: {}".format((min(x_list) + max(x_list))/2, (min(y_list) + max(y_list))/2))
 finally:
     compass.setNormalSpeedDataRate()
     compass.sleep()
