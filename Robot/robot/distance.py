24266438365d4e723634f31341fbaeee6f09110b
gkraften <g.spam32@gmail.com>
2015-11-28 11:37:09 +0100

    Strukturerade modulen med avståndssensor

--- Robot/robot/distance.py
+++ Robot/robot/distance.py
@@ -2,36 +2,30 @@ import robot.pins as pins
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setup(pins.DISTANCE_MID_TRIG, GPIO.OUT)
-GPIO.setup(pins.DISTANCE_MID_ECHO, GPIO.IN)
+class Rangefinder:
+    def __init__(self, trig, echo):
+        self.trig = trig
+        self.echo = echo
 
-GPIO.setup(pins.DISTANCE_LEFT_TRIG, GPIO.OUT)
-GPIO.setup(pins.DISTANCE_LEFT_ECHO, GPIO.IN)
+        GPIO.setup(trig, GPIO.OUT, initial=False)
+        GPIO.setup(echo, GPIO.IN)
 
-GPIO.setup(pins.DISTANCE_RIGHT_TRIG, GPIO.OUT)
-GPIO.setup(pins.DISTANCE_RIGHT_ECHO, GPIO.IN)
+    def distance(self):
+        GPIO.output(self.trig, True)
+        time.sleep(0.00001)
+        GPIO.output(self.trig, False)
 
-def get(trig, echo):
-    GPIO.output(trig, 1)
-    time.sleep(0.00001)
-    GPIO.output(trig, 0)
+        while GPIO.input(self.echo) == 0:
+            pass
+        start = time.time()
 
-    while GPIO.input(echo) == 0:
-        pass
-    start = time.time()
+        while GPIO.input(self.echo) == 1:
+            pass
 
-    while GPIO.input(echo) == 1:
-        pass
+        stop = time.time()
 
-    stop = time.time()
+        return (stop - start) * 17150
 
-    return (stop - start) * 17150
-
-def get_mid():
-    return get(pins.DISTANCE_MID_TRIG, pins.DISTANCE_MID_ECHO)
-
-def get_left():
-    return get(pins.DISTANCE_LEFT_TRIG, pins.DISTANCE_LEFT_ECHO)
-
-def get_right():
-    return get(pins.DISTANCE_RIGHT_TRIG, pins.DISTANCE_RIGHT_ECHO)
+LEFT = Rangefinder(pins.DISTANCE_LEFT_TRIG, pins.DISTANCE_LEFT_ECHO)
+MIDDLE = Rangefinder(pins.DISTANCE_MID_TRIG, pins.DISTANCE_MID_ECHO)
+RIGHT = Rangefinder(pins.DISTANCE_RIGHT_TRIG, pins.DISTANCE_RIGHT_ECHO)
