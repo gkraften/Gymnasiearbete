d0e9c70b39052aed6315b3968b69a7941c620673
Oscar <g.spam32@gmail.com>
2015-12-01 14:22:11 +0100

    Mät avstånd

--- Robot/halleffekt.py
+++ Robot/halleffekt.py
@@ -2,17 +2,20 @@ import RPi.GPIO as GPIO
 import robot.motors
 import time
 
+distance = 0
+
+def callback(a):
+    global distance
+    distance += 10.21
+
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+GPIO.add_event_detect(7, GPIO.RISING, callback=callback, bouncetime=200)
+
 robot.motors.forward(50)
-distance = 0
 try:
     while True:
-        if GPIO.input(7) == 0:
-            distance += 10.21
-            print("mer")
-            while GPIO.input(7) == 0:
-                pass
+        pass
 except KeyboardInterrupt:
     print(distance)
 finally:
