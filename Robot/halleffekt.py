a0b33bdca3a825df75229ed236894af1e9bfec83
Oscar <g.spam32@gmail.com>
2015-12-01 14:10:20 +0100

    Mät avstånd

--- Robot/halleffekt.py
+++ Robot/halleffekt.py
@@ -5,9 +5,12 @@ import time
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 robot.motors.forward(30)
-
+distance = 0
 try:
     while True:
-        print(GPIO.input(7))
+        if GPIO.input(7) == 0:
+            distance += 10,21
+        while GPIO.input(7) == 0:
+            pass
 except:
     robot.clean()
