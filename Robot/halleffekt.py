9ac6a7155dae78da6d90c5a26c073b8295c0b3ba
Oscar <g.spam32@gmail.com>
2015-12-03 12:52:21 +0100

    Hall-effekt

--- Robot/halleffekt.py
+++ Robot/halleffekt.py
@@ -16,7 +16,11 @@ GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 robot.motors.forward(50)
 try:
     while True:
+        while GPIO.input(7) == 1:
+            pass
         print(GPIO.input(7))
+        while GPIO.input(7) == 0:
+            pass
 except KeyboardInterrupt:
     print(distance)
 finally:
