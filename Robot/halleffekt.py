0904cbe980275363a58d450166f7522e2ac2262d
Oscar <g.spam32@gmail.com>
2015-12-03 13:33:49 +0100

    Hall-effekt

--- Robot/halleffekt.py
+++ Robot/halleffekt.py
@@ -11,12 +11,14 @@ def callback(a):
 
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.add_event_detect(7, GPIO.FALLING, callback=callback, bouncetime=100)
+#GPIO.add_event_detect(7, GPIO.FALLING, callback=callback, bouncetime=100)
 
 robot.motors.forward(30)
 try:
     while True:
-        pass
+        GPIO.wait_for_edge(7, GPIO.FALLING)
+        distance += 1
+        print("distance")
 except KeyboardInterrupt:
     print(distance)
 finally:
