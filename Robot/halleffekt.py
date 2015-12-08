dfd4a3eef01dbc61168af238c61e558fc9399e7f
Oscar <g.spam32@gmail.com>
2015-12-08 19:00:14 +0100

    Hall-effekt

--- Robot/halleffekt.py
+++ Robot/halleffekt.py
@@ -1,19 +1,13 @@
 import RPi.GPIO as GPIO
 import robot.motors
 import robot
-import random
 import time
 
 distance = 0
 
-def callback(a):
-    global distance
-    distance += 1
-
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#GPIO.add_event_detect(7, GPIO.FALLING, callback=callback)
 
-robot.motors.LEFT.forward(30)
+robot.motors.LEFT.forward(100)
 try:
     last = 0
     while True:
