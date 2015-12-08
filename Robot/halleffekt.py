060a0765f329c36a430aa6a72d9e8f9576daa678
Oscar <g.spam32@gmail.com>
2015-12-08 18:44:28 +0100

    Hall-effekt

--- Robot/halleffekt.py
+++ Robot/halleffekt.py
@@ -12,7 +12,7 @@ def callback(a):
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 #GPIO.add_event_detect(7, GPIO.FALLING, callback=callback)
 
-robot.motors.LEFT.forward(30)
+robot.motors.LEFT.forward(100)
 try:
     while True:
         while GPIO.input(7) == 1:
