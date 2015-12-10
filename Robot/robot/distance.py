4dfd674349dc5dae0c9fecbf91ce08126a952e2f
Oscar <g.spam32@gmail.com>
2015-12-10 14:22:36 +0100

    Mät avstånd

--- Robot/robot/distance.py
+++ Robot/robot/distance.py
@@ -1,5 +1,35 @@
 import RPi.GPIO as GPIO
 import robot.pins as pins
 from threading import Thread
+import time
 
 GPIO.setup(pins.HALL_EFFECT, GPIO.IN)
+
+_meassuring = False
+_d = 0
+
+HALF_CIRCUMFERENCE = 10.21
+
+def _meassure(callback):
+    global _d
+    last = 0
+    while _meassuring:
+        if GPIO.input(pins.HALL_EFFECT) == 0:
+            dt = time.time() - last
+            if dt < 0.15:
+                _d += HALF_CIRCUMFERENCE
+                if not callback is None:
+                    callback()
+
+
+def start_meassuring(callback=None):
+    if not _meassuring:
+        t = Thread(target=_meassure, args=(callback,))
+        t.start()
+
+def stop_meassuring():
+    _meassuring = False
+    _d = 0
+
+def get_distance():
+    return _d
