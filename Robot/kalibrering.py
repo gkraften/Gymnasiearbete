df3665e46be250b2d03f59a2400495c3e661d529
ab53995 <jonas.cederberg@gmail.com>
2015-12-10 14:23:20 +0100

    gyarb

--- Robot/kalibrering.py
+++ Robot/kalibrering.py
@@ -2,3 +2,29 @@ import RPi.GPIO as GPIO
 import robot.motors
 import robot
 import time
+import robot.compass as compass
+
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+left = 100
+right = 100
+direction = 0
+new_direction = 0
+
+done = True
+try:
+    while done:
+        robot.motors.LEFT.forward(left)
+        robot.motors.RIGHT.forward(right)
+        direction = compass.getHeading
+        time.sleep(1)
+        new_direction = compass.getHeading
+        if direction - new_direction > 0:
+            right -= 5
+        if direction - new_direction < 0:
+            left -= 5
+        # grader norr från motsols
+except KeyboardInterrupt:
+    print("Höger kör", right, "Vänster kör", left)
+finally:
+    robot.clean()
