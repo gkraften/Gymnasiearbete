6e37ce95f9eac18158bd7490fb4e9c65193dbd4a
Oscar <g.spam32@gmail.com>
2015-12-12 19:07:47 +0100

    Ändra tid

--- Robot/kalibrering.py
+++ Robot/kalibrering.py
@@ -18,7 +18,7 @@ try:
         robot.motors.LEFT.forward(left)
         robot.motors.RIGHT.forward(right)
         direction = compass.getHeading()
-        time.sleep(0.5)
+        time.sleep(1)
         new_direction = compass.getHeading()
         if compass.angleDifference(new_direction, direction) < -math.radians(0.1):
             left -= 5
