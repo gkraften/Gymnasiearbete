544c4f04196d8e18c35b6489748c0ca63f226742
Oscar <g.spam32@gmail.com>
2015-12-12 19:06:51 +0100

    Ändra felmarginal

--- Robot/kalibrering.py
+++ Robot/kalibrering.py
@@ -13,34 +13,22 @@ right = 100
 direction = 0
 new_direction = 0
 
-done = True
-
-def wheel_turned():
-    global done
-    done = False
-
 try:
-    robot.motors.forward(100)
-    robot.distance.start_meassuring(wheel_turned)
-    while done:
-        pass
-    robot.distance.stop_meassuring()
-    while not done:
+    while True:
         robot.motors.LEFT.forward(left)
         robot.motors.RIGHT.forward(right)
         direction = compass.getHeading()
-        time.sleep(1)
+        time.sleep(0.5)
         new_direction = compass.getHeading()
         if compass.angleDifference(new_direction, direction) < -math.radians(0.1):
             left -= 5
         elif compass.angleDifference(new_direction, direction) > math.radians(0.1):
             right -= 5
         elif abs(compass.angleDifference(new_direction, direction)) < math.radians(0.1):
-            done = True
+            break
         # grader norr från motsols
 except KeyboardInterrupt:
-    pass
+    print("Avbryter")
 finally:
-    if done:
-        print("Höger kör {}\tVänster kör {}".format(right, left))
+    print("Höger kör {}\tVänster kör {}".format(right, left))
     robot.clean()
