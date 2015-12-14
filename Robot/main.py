aa60443a1d73b2ada40e598d4d51e013b2523b8f
Oscar <g.spam32@gmail.com>
2015-12-14 16:39:36 +0100

    Avstånd

--- Robot/main.py
+++ Robot/main.py
@@ -7,29 +7,6 @@ import vector
 import time
 import sys
 
-pos = Vector(0, 0)
-
-distance.start_measuring()
 motors.forward()
 time.sleep(5)
-motors.stop()
-after = compass.getHeading()
-distance.stop_measuring()
-pos += vector.from_polar(distance.get_distance(), after)
-print("{}\t{}".format(distance.get_distance(), pos.length()))
-robot.clean()
-sys.exit()
-
-motors.right(50)
-time.sleep(1)
-motors.stop()
-
-distance.start_measuring()
-motors.forward()
-time.sleep(2)
-motors.stop()
-after = compass.getHeading()
-distance.stop_measuring()
-pos += vector.from_polar(distance.get_distance(), after)
-
-print("Avstånd från start: {}".format(pos.length()))
+motors.clean()
