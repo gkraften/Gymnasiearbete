c44cd687ede8e3393c9582c6cd21cab7d6ab11fe
Oscar <g.spam32@gmail.com>
2015-12-17 13:39:36 +0100

    Hej

--- Robot/main.py
+++ Robot/main.py
@@ -10,9 +10,12 @@ import sys
 robot.on_battery_low(robot.halt)
 
 pos = Vector(0, 0)
+d = 0
 
 def f():
     global pos
+    global d
+    d += 1
     pos += vector.from_polar(distance.HALF_CIRCUMFERENCE, compass.getHeading())
 
 distance.start_measuring(f)
@@ -22,4 +25,5 @@ motors.stop()
 distance.stop_measuring()
 
 print(pos.length())
+print(d)
 robot.clean()
