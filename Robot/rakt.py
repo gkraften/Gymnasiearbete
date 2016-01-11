47c2e18fbaba68f73909fc7540646013a3ca37c6
gkraften <g.spam32@gmail.com>
2016-01-11 17:27:25 +0100

    Oftare

--- Robot/rakt.py
+++ Robot/rakt.py
@@ -36,7 +36,7 @@ try:
         motors.LEFT.forward(l_speed)
         motors.RIGHT.forward(r_speed)
 
-        time.sleep(0.5)
+        time.sleep(0.25)
 except:
     print("Vänster: {}%\tHöger: {}%".format(l_speed, r_speed))
     robot.clean()
