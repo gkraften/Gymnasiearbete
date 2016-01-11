fb21650aecce5e8c381f8b61b75455b6e3b28891
gkraften <g.spam32@gmail.com>
2016-01-11 16:54:56 +0100

    Vänd på tecken

--- Robot/rakt.py
+++ Robot/rakt.py
@@ -29,11 +29,11 @@ try:
         ret = pid.update(last_direction - direction, dt)
         last_direction = direction
 
-        if ret < 0:
-            r_speed = 100
-            l_speed = 100 + ret
         if ret > 0:
-            r_speed = 100 - ret
+            r_speed = 100
+            l_speed = 100 - ret
+        if ret < 0:
+            r_speed = 100 + ret
             l_speed = 100
 
         motors.LEFT.forward(l_speed)
