f57317fe7d4c2e3765ac53395b32e96d9fbc8c99
gkraften <g.spam32@gmail.com>
2016-01-11 18:01:56 +0100

    Oftare

--- Robot/rakt.py
+++ Robot/rakt.py
@@ -37,7 +37,7 @@ try:
         motors.LEFT.forward(l_speed)
         motors.RIGHT.forward(r_speed)
 
-        time.sleep(0.5)
+        time.sleep(0.25)
 except KeyboardInterrupt:
     print("Vänster: {}%\tHöger: {}%".format(l_speed, r_speed))
 finally:
