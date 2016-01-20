9cf1adf69b1231c7b8d25b78f4db5739a0cd11b8
Oscar <g.spam32@gmail.com>
2016-01-20 17:57:15 +0100

    Testa snurr

---
+++ Robot/testa_snurr.py
@@ -0,0 +1,18 @@
+import robot
+from robot import motors
+from robot import compass
+import math
+
+compass.calibrate(10)
+
+cmd = input("skriv gradtal, kp, ki, kd (q för att avsluta) ")
+try:
+    while cmd != "q":
+        nums = cmd.split(" ")
+        robot.turn_to(math.radians(math.float(nums[0])), float(nums[1]), float(nums[2]), float(nums[3]))
+        cmd = input("skriv gradtal, kp, ki, kd (q för att avsluta) ")
+except KeyboardInterrupt:
+    pass
+finally:
+    motors.stop()
+    robot.clean()
