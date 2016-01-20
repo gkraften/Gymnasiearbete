bf3697884ea9cd0c8192d1182c2851f7a2dd920f
Oscar <g.spam32@gmail.com>
2016-01-20 17:58:25 +0100

    Testa snurr

--- Robot/testa_snurr.py
+++ Robot/testa_snurr.py
@@ -9,7 +9,7 @@ cmd = input("skriv gradtal, kp, ki, kd (q för att avsluta) ")
 try:
     while cmd != "q":
         nums = cmd.split(" ")
-        robot.turn_to(math.radians(math.float(nums[0])), float(nums[1]), float(nums[2]), float(nums[3]))
+        robot.turn_to(math.radians(float(nums[0])), float(nums[1]), float(nums[2]), float(nums[3]))
         cmd = input("skriv gradtal, kp, ki, kd (q för att avsluta) ")
 except KeyboardInterrupt:
     pass
