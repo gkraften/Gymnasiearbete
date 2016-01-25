b79160c149ebeb54628de76b4f9851f976997e8a
Oscar <g.spam32@gmail.com>
2016-01-25 13:42:34 +0100

    Debug

--- Robot/full_test.py
+++ Robot/full_test.py
@@ -4,12 +4,17 @@ import robot
 import math
 
 try:
+    print("Kalibrerar")
     compass.calibrate(5)
+    print("Snurrar mot väst")
     robot.turn_to(math.pi / 2)
+    print("Åker frammåt")
     motors.forward()
     input("Du är fett ful")
     motors.stop()
+    print("Snurrar mot öst")
     robot.turn_to(3*math.pi/2)
+    print("Åker framåt")
     motors.forward()
 except KeyboardInterrupt:
     pass
