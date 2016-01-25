d6094b50a1cb379512ed552c2c321b8f4febc183
Oscar <g.spam32@gmail.com>
2016-01-25 14:10:08 +0100

    Mät avstånd

--- Robot/full_test.py
+++ Robot/full_test.py
@@ -1,19 +1,27 @@
 from robot import compass
 from robot import motors
+from robot import distance
 import robot
 import math
 
+n = 0
+
+def count():
+    global d
+    d += 1
+
 try:
     compass.calibrate(5)
-    while True:
-        robot.turn_to(math.pi / 2, math.radians(4))
-        motors.forward(math.pi / 2)
-        input("Du är fett ful")
-        motors.stop()
-        robot.turn_to(3*math.pi/2, math.radians(4))
-        motors.forward(3*math.pi/2)
-        input("VÄND FÖR I HELVETE!!!")
-        motors.stop()
+    robot.turn_to(math.pi/2)
+    distance.start_measuring(count)
+    motors.forward(math.pi/2)
+    input("Tryck på enter")
+    motors.stop()
+    distance.stop_measuring()
+    print("Antal halva varv: {}".format(d))
+    robot.turn_to(3*math.pi/2)
+    robot.forward(3*math.pi/2)
+    input("Tryck på enter för att stanna")
 finally:
     motors.stop()
     robot.clean()
