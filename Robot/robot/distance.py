1b08f0ba59725a2395c1e3b47c08d0ea0a2752a3
Oscar <g.spam32@gmail.com>
2016-01-25 14:58:48 +0100

    Blbla

--- Robot/robot/distance.py
+++ Robot/robot/distance.py
@@ -22,7 +22,7 @@ def _measure():
         now = GPIO.input(pins.HALL_EFFECT)
 
         if now == 0 and sum(last) == 15:
-            print(sum(list))
+            print(sum(last))
             _callback()
 
         for i in range(14, 0):
