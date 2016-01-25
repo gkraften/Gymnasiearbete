328c8a31b6b4f3393128b8ce8e85d11792ba814e
Oscar <g.spam32@gmail.com>
2016-01-25 13:57:47 +0100

    Större felmarginal

--- Robot/full_test.py
+++ Robot/full_test.py
@@ -6,11 +6,11 @@ import math
 try:
     compass.calibrate(5)
     while True:
-        robot.turn_to(math.pi / 2)
+        robot.turn_to(math.pi / 2, math.radians(4))
         motors.forward()
         input("Du är fett ful")
         motors.stop()
-        robot.turn_to(3*math.pi/2)
+        robot.turn_to(3*math.pi/2, math.radians(4))
         motors.forward()
         input("VÄND FÖR I HELVETE!!!")
         motors.stop()
