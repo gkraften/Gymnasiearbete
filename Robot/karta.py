8bc766b977eb386b6b2eb5d3e217871dfa2fe98d
Oscar <g.spam32@gmail.com>
2016-01-28 14:52:37 +0100

    Fler punkter

--- Robot/karta.py
+++ Robot/karta.py
@@ -4,6 +4,7 @@ from robot import range
 import robot
 import vector
 import time
+import math
 
 f = open("map.txt", "w")
 
@@ -11,8 +12,15 @@ try:
     compass.calibrate(5)
     motors.left(70)
     while True:
-        v = vector.from_polar(range.MIDDLE.distance(), compass.getHeading())
+        u = vector.from_polar(range.MIDDLE.distance(), compass.getHeading())
+        print("{},{}".format(u.x, u.y), file=f)
+
+        v = vector.from_polar(range.LEFT.distance(), compass.angleDifference(compass.getHeading(), -math.pi/4))
         print("{},{}".format(v.x, v.y), file=f)
+
+        w = vector.from_polar(range.RIGHT.distance(), compass.angleDifference(compass.getHeading(), math.pi/4))
+        print("{},{}".format(w.x, w.y), file=f)
+
         time.sleep(0.1)
 finally:
     motors.stop()
