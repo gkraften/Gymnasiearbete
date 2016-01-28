ce5ecf564589448a4ac6be06f97c3fe692b74d18
Oscar <g.spam32@gmail.com>
2016-01-28 14:36:53 +0100

    Gör karta

---
+++ Robot/karta.py
@@ -0,0 +1,20 @@
+from robot import compass
+from robot import motors
+from robot import range
+import robot
+import vector
+import time
+
+f = open("map.txt", "w")
+
+try:
+    compass.calibrate(5)
+    motors.left(50)
+    while True:
+        v = vector.from_polar(range.MIDDLE.distance(), compass.getHeading())
+        print("{},{}".format(v.x, v.y), file=f)
+        time.sleep(0.1)
+finally:
+    motors.stop()
+    robot.clean()
+    f.close()
