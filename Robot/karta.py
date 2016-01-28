7776558199456732f07fec3ae5b6d6f2e450b52b
Oscar <g.spam32@gmail.com>
2016-01-28 15:16:28 +0100

    Bara två varv

--- Robot/karta.py
+++ Robot/karta.py
@@ -14,7 +14,9 @@ f = open("/var/www/map.txt", "w")
 try:
     compass.calibrate(5)
     motors.left(60)
-    while True:
+    turns = -1
+    start = compass.getHeading()
+    while turns < 2:
         u = vector.from_polar(range.MIDDLE.distance(), compass.getHeading())
         print("{},{}".format(u.x, u.y), file=f)
 
@@ -23,6 +25,10 @@ try:
 
         w = vector.from_polar(range.RIGHT.distance(), compass.angleDifference(compass.getHeading(), math.pi/4))
         print("{},{}".format(w.x, w.y), file=f)
+
+        dtheta = compass.angleDifference(compass.getHeading(), start)
+        if dtheta > math.pi/4 and dtheta < math.pi:
+            turns += 1
 
         time.sleep(0.1)
 finally:
