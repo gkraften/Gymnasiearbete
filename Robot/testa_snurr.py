763e2776c28803fee60a84cb4b32f2e7e5b0989d
Oscar <g.spam32@gmail.com>
2016-01-29 15:56:24 +0100

    Visa rotation

--- Robot/testa_snurr.py
+++ Robot/testa_snurr.py
@@ -2,14 +2,25 @@ import robot
 from robot import motors
 from robot import compass
 import math
+import maplogger
+from threading import Thread
+import time
 
 compass.calibrate(10)
+
+maplogger.initialize()
 
 cmd = input("skriv gradtal (q för att avsluta) ")
 try:
     while cmd != "q":
         nums = cmd.split(" ")
-        robot.turn_to(math.radians(float(nums[0])))
+        turner = Thread(target=robot.turn_to, args=(math.radians(float(nums[0])), math.radians(4)))
+        turner.start()
+
+        while turner.isAlive():
+            maplogger.log(heading=compass.getHeading())
+            time.sleep(0.1)
+
         print("Fel: {}°".format(math.degrees(compass.angleDifference(math.radians(float(nums[0])), compass.getHeading()))))
         cmd = input("skriv gradtal (q för att avsluta) ")
 except KeyboardInterrupt:
@@ -17,3 +28,4 @@ except KeyboardInterrupt:
 finally:
     motors.stop()
     robot.clean()
+    maplogger.close()
