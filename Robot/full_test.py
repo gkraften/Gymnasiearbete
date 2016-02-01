ae093db800235f3b7ccaecc7c187fc2b5843f4eb
Oscar <g.spam32@gmail.com>
2016-02-01 10:52:02 +0100

    Åk mer komplicerat

--- Robot/full_test.py
+++ Robot/full_test.py
@@ -51,6 +51,33 @@ try:
         else:
             maplogger.log(position=[[n.x, n.y]], heading=compass.getHeading(), walls=data)
         time.sleep(0.5)
+    motors.stop()
+    distance.stop_measuring()
+    robot.turn_to(0, math.radians(4))
+    motors.forward(0)
+    for i in range(6):
+        m = ultrasonic.get_middle()
+        u = vector.from_polar(m, compass.getHeading())
+
+        l = ultrasonic.get_left()
+        v = vector.from_polar(l, compass.angleDifference(compass.getHeading(), -math.radians(30)))
+
+        r = ultrasonic.get_right()
+        w = vector.from_polar(r, compass.angleDifference(compass.getHeading(), math.radians(30)))
+
+        data = []
+        if m <= 400:
+            data.append([u.x, u.y])
+        if l <= 400:
+            data.append([v.x, v.y])
+        if r <= 400:
+            data.append([w.x, w.y])
+
+        if len(data) == 0:
+            maplogger.log(position=[[n.x, n.y]], heading=compass.getHeading())
+        else:
+            maplogger.log(position=[[n.x, n.y]], heading=compass.getHeading(), walls=data)
+        time.sleep(0.5)
 
 finally:
     distance.stop_measuring()
