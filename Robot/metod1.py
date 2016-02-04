61b051b3cd0ed4d374e15f8ed3c89b26af0409df
Oscar <g.spam32@gmail.com>
2016-02-04 13:47:41 +0100

    Den måste ju åka framåt

--- Robot/metod1.py
+++ Robot/metod1.py
@@ -105,7 +105,7 @@ try:
                 logger = threading.Thread(target=supermagiskt)
                 logger.start()
                 time.sleep(1)
-                print("Din mamma är fett ful")
+                motors.forward()
             t = time.time()
             last = compass.getHeading()
         time.sleep(0.1)
