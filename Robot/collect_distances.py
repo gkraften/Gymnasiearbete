565fc076fb9ed25812b72378b26e89c3aea22cc0
oscar <g.spam32@gmail.com>
2016-03-07 16:46:38 +0100

    Samla data

---
+++ Robot/collect_distances.py
@@ -0,0 +1,13 @@
+from robot import ultrasonic
+import robot
+from time import sleep
+
+n = int(input("Hur många punkter från varje sensor?"))
+
+with open("distances.csv", "w") as f:
+    for i in range(n):
+        d1 = ultrasonic._LEFT.distance()
+        d2 = ultrasonic._MIDDLE.distance()
+        d3 = ultrasonic._RIGHT.distance()
+        print("{},{},{}".format(d1, d2, d3), file=f)
+        sleep(0.05)
